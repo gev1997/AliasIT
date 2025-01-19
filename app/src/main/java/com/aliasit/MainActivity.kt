342bc472098c8a4fad76e7cfba1719ea429a5e72
Gevorg <qamalyan86@gmail.com>
2025-01-19 23:47:34 +0400

    Add TeamsViewModel to MainActivity

--- app/src/main/java/com/aliasit/MainActivity.kt
+++ app/src/main/java/com/aliasit/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.aliasit.ui.theme.AliasITTheme
+import com.aliasit.viewmodels.TeamsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AliasITTheme {
+                mTeamsViewModel = viewModel()
                 mNavController = NavController(rememberNavController())
 
                 NavHost(mNavController.getNavHostController(), "Home") {
@@ -32,5 +35,10 @@ class MainActivity : ComponentActivity() {
         return mNavController
     }
 
+    fun getTeamsViewModel(): TeamsViewModel {
+        return mTeamsViewModel
+    }
+
     private lateinit var mNavController: NavController
+    private lateinit var mTeamsViewModel: TeamsViewModel
 }
