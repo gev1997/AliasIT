6c6200b50ecc59470d9f41a7ecad5da63201fe78
Gevorg <qamalyan86@gmail.com>
2025-02-18 00:21:47 +0400

    GameWinPage() initial design and functionality

--- app/src/main/java/com/aliasit/ui/GameWinPage.kt
+++ app/src/main/java/com/aliasit/ui/GameWinPage.kt
@@ -1,7 +1,65 @@
 package com.aliasit.ui
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainActivity.GameWinPage() {
+    val teams = getTeamsViewModel().getStateFlow().collectAsState().value
+    val currentTeam = teams.getCurrentTeam()
+
+    Scaffold { padding ->
+        BoxWithConstraints {
+            val mainContentColumnWidth = maxWidth * 0.9f
+
+            // Main Column
+            Column(Modifier.fillMaxSize().background(backgroundColor).padding(padding), horizontalAlignment = Alignment.CenterHorizontally) {
+                // Main content Column
+                Column(Modifier.fillMaxHeight().width(mainContentColumnWidth).verticalScroll(rememberScrollState()), Arrangement.SpaceBetween) {
+                    // Title Column
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {}
+
+                    // Next Team/Player Column
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text("Claim victory", color = textColor, fontSize = 28.sp)
+                        Text(currentTeam.getName(), color = textColor, fontSize = 24.sp)
+                    }
+
+                    Utility.Spacer(0, 2)
+
+                    // Back Column
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Button({ backToHomeHandler() }, Modifier.fillMaxWidth(), colors = ButtonDefaults.buttonColors(buttonColor), shape = buttonShape) {
+                            Text("Back", color = textColor, fontSize = 28.sp)
+                        }
+
+                        Utility.Spacer(10)
+                    }
+                }
+            }
+        }
+    }
+
+    BackHandler(true) {
+        backToHomeHandler()
+    }
 }
